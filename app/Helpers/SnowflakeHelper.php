a072ad0b3b86aa0acc4a17d86f02d3a508fb94cf
Naveen <nchand041989@gmail.com>
2025-02-18 15:35:32 +0530

    Updated Snoflake Helper to avoid collisions

--- app/Helpers/SnowflakeHelper.php
+++ app/Helpers/SnowflakeHelper.php
@@ -4,9 +4,14 @@ use App\Services\Snowflake;
 
 function generate_snowflake_id(int $machineId = null): int
 {
+    static $snowflake;
 
-    $machineId = $machineId ?? env('SNOWFLAKE_MACHINE_ID', 1);      /** If no machine ID is passed, use the one from the environment */
-    $snowflake = new Snowflake($machineId);
+    // Initialize the Snowflake instance if it's not already created
+    if (!$snowflake) {
+        $machineId = $machineId ?? env('SNOWFLAKE_MACHINE_ID', 1);  // Default machine ID from environment
+        $snowflake = new Snowflake($machineId);
+    }
+
     return $snowflake->generateId();
 }
 
