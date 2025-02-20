a142ba559cc5cb213be19e56e6ecd31b3f74daa6
Naveen <nchand041989@gmail.com>
2025-02-20 09:51:20 +0530

    Updated New Rules

--- app/Helpers/OrderRule.php
+++ app/Helpers/OrderRule.php
@@ -84,4 +84,66 @@ class OrderRule
         });
     }
 
+
+    public static function R100000000000200004($webhookid, $strategyid, $exchangeid, $currencyid, $ruleId, $walletId, $timeframe, $price, $positionSize, $action)
+    {
+        if (is_null($positionSize) || is_null($price) || !in_array($action, ['buy', 'sell'])) {
+            return response()->json(
+                [
+                    'message'   =>  'Invalid data'
+                ], 400);
+        }
+
+        $positionType = $action === 'buy' ? 'long' : 'short';
+
+        return DB::transaction(function () use ($webhookid, $strategyid, $exchangeid, $currencyid, $ruleId, $walletId, $timeframe, $price, $positionSize, $positionType) {
+            $closeTradeResponse = CloseTradeHelper::CloseTrade($webhookid, $strategyid, $exchangeid, $currencyid, $ruleId, $walletId, $price);
+            $responseData = $closeTradeResponse->getData();
+
+            if ($positionType == 'long') {
+                $openTradeResponse = OpenTradeHelper::openTrade($webhookid, $strategyid, $exchangeid, $currencyid, $ruleId, $walletId, $timeframe, $price, $positionSize, $positionType);
+            } else {
+                $openTradeResponse = null;
+            }
+            
+
+            return response()->json([
+                'message'       =>  'Trade processed successfully',
+                'close_trade'   =>  $closeTradeResponse,
+                'open_trade'    =>  $openTradeResponse
+            ], 200);
+        });
+    }
+
+
+    public static function R100000000000200005($webhookid, $strategyid, $exchangeid, $currencyid, $ruleId, $walletId, $timeframe, $price, $positionSize, $action)
+    {
+        if (is_null($positionSize) || is_null($price) || !in_array($action, ['buy', 'sell'])) {
+            return response()->json(
+                [
+                    'message'   =>  'Invalid data'
+                ], 400);
+        }
+
+        $positionType = $action === 'buy' ? 'long' : 'short';
+
+        return DB::transaction(function () use ($webhookid, $strategyid, $exchangeid, $currencyid, $ruleId, $walletId, $timeframe, $price, $positionSize, $positionType) {
+            $closeTradeResponse = CloseTradeHelper::CloseTrade($webhookid, $strategyid, $exchangeid, $currencyid, $ruleId, $walletId, $price);
+            $responseData = $closeTradeResponse->getData();
+
+            if ($positionType == 'short') {
+                $openTradeResponse = OpenTradeHelper::openTrade($webhookid, $strategyid, $exchangeid, $currencyid, $ruleId, $walletId, $timeframe, $price, $positionSize, $positionType);
+            } else {
+                $openTradeResponse = null;
+            }
+            
+
+            return response()->json([
+                'message'       =>  'Trade processed successfully',
+                'close_trade'   =>  $closeTradeResponse,
+                'open_trade'    =>  $openTradeResponse
+            ], 200);
+        });
+    }
+
 }
--- database/seeders/RulesTableSeeder.php
+++ database/seeders/RulesTableSeeder.php
@@ -10,7 +10,7 @@ class RulesTableSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('rules')->insert([
+        $rules = [
             [
                 'rid'         => env('RULE_NO_CONDITION', 100000000000200001),
                 'name'        => 'No Conditions',
@@ -32,6 +32,23 @@ class RulesTableSeeder extends Seeder
                 'created_at'  => Carbon::now(),
                 'updated_at'  => Carbon::now(),
             ],
-        ]);
+            [
+                'rid'         => env('ONLY_LONG_TRADE', 100000000000200004),
+                'name'        => 'Only Long Orders',
+                'status'      => 'Active',
+                'created_at'  => Carbon::now(),
+                'updated_at'  => Carbon::now(),
+            ],
+            [
+                'rid'         => env('ONLY_SHORT_TRADE', 100000000000200005),
+                'name'        => 'Only Short Orders',
+                'status'      => 'Active',
+                'created_at'  => Carbon::now(),
+                'updated_at'  => Carbon::now(),
+            ],
+        ];
+
+        // Insert the rules while ignoring any duplicates based on 'rid'
+        DB::table('rules')->insertOrIgnore($rules);
     }
 }
